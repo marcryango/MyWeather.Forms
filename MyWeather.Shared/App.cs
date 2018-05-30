926279f693127c73d72bae0865172a323b0b2e91
AMAIISDOM\gom <marc.ryan.t.go@navitaire.com>
2018-05-30 15:27:51 +0800

    added analytics
    added analytics

--- MyWeather.Shared/App.cs
+++ MyWeather.Shared/App.cs
@@ -1,4 +1,6 @@
-﻿using MyWeather.View;
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using MyWeather.View;
 using MyWeather.ViewModels;
 using Xamarin.Forms;
 using static System.Diagnostics.Debug;
@@ -33,6 +35,11 @@ namespace MyWeather
         protected override void OnStart()
         {
             base.OnStart();
+
+            Microsoft.AppCenter.AppCenter.Start("android=df0f03f9-103c-42c5-b68d-a7d2f5aab1fc" ,
+                  //"uwp={Your UWP App secret here};" +
+                  //"ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
 
             WriteLine("Application OnStart");
         }
